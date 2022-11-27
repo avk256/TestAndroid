b6d3c18c3c7749001b396c135457f55e9223e728
Alexanyan <1wndb4@gmail.com>
2022-11-27 22:35:31 +0200

    MainActivityTest(unit tests) - Fix testArray2dict(). Add testSetTestMode()

--- app/src/test/java/com/example/Lab3/MainActivityTest.java
+++ app/src/test/java/com/example/Lab3/MainActivityTest.java
@@ -1,32 +1,58 @@
 package com.example.Lab3;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.content.SharedPreferences;
+
 import junit.framework.TestCase;
-import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Map;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityTest extends TestCase {
 
     public String[] tags = new String[]{"Червоний:-65536", "Зелений:-144357", "Помаранчевий:-26368",
         "Жовтий:-598252", "Блакитний:-16728876", "Синій:-14401322", "Фіолетовий:-9422635",
         "Чорний:-16777216", "Рожевий:-841281027", };
 
-    public void setUp() throws Exception {
-        super.setUp();
-    }
+    @Mock
+    SharedPreferences mockSharedPreferences;
 
-    public void tearDown() throws Exception {
-    }
+    @Mock
+    SharedPreferences.Editor mockEditor;
 
     public void testOnCreate() {
     }
+
     @Test
     public void testArray2dict() {
         Map map = MainActivity.array2dict(tags);
         int map_length = map.size();
-        assertTrue(map_length == 8);
+        assertTrue(map_length == 9);
+    }
+
+    @Test
+    public void testSetTestMode() {
+        // Return the MockEditor when requesting it.
+        when(mockSharedPreferences.edit()).thenReturn(mockEditor);
+        when(mockEditor.putInt(anyString(), anyInt())).thenReturn(mockEditor);
+
+        int status = 1;
+        MainActivity.setTestMode(mockSharedPreferences, status);
+
+        // check if editor.putInt() is called with parameters passed to setTestMode()
+        verify(mockEditor).putInt("testing_mode", status);
+        // check if editor.apply() is called
+        verify(mockEditor).apply();
     }
 
     public void testOnYesClick() {
